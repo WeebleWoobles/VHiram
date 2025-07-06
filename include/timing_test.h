a48a905fc322c4110897a60f430d4f90c89eec4e
JaxonTerrace22 <jaxonterrace@gmail.com>
2025-07-06 15:29:24 -0600

    Update timing_test.h

--- include/timing_test.h
+++ include/timing_test.h
@@ -1,6 +1,9 @@
 #ifndef TIMING_TEST_H
 #define TIMING_TEST_H
 
+#include <stdbool.h>
+
+// Function prototype for the timing test FreeRTOS task
 void timing_test_task(void* pvParameters);
 
-#endif
+#endif // TIMING_TEST_H
